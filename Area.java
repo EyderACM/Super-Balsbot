bf715676faea1c04a4856dcfe10fa647c27b318c
RafaelCantoVazquez <44030094+RafaelCantoVazquez@users.noreply.github.com>
2019-10-02 10:56:26 -0500

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author Rafael
+ */
+public class Area {
+    private String areaName;
+    private String areaId;
+    Map<String, Room> Rooms = new HashMap<>();
+    
+}
---
+++ Room.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author Rafael
+ */
+public class Room {
+    private String roomName;
+    private String roomId;
+    Map<String, Device> Devices = new HashMap<();
+    
+}
