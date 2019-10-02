a0c0cfdc687cb5a0650e5894a399f9b72c9501a7
RafaelCantoVazquez <44030094+RafaelCantoVazquez@users.noreply.github.com>
2019-10-02 11:11:49 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -12,6 +12,27 @@ import java.util.Map;
 public class Room {
     private String roomName;
     private String roomId;
-    Map<String, Device> Devices = new HashMap<();
+    Map<String, Device> Devices = new HashMap<>();
     
+    public void setRoomName(String roomName){
+        this.roomName = roomName;
+    }
+    public void setRoomId(String roomId){
+        this.roomId = roomId;
+    }
+    public String getRoomName(){
+        return roomName;
+    }
+    public String getRoomId(){
+        return roomId;
+    }
+    
+    Room(String roomName, String roomId){
+        this.roomName = roomName;
+        this.roomId = roomId;
+    }
+    
+    public void addDevice(Device newDevice){
+        Devices.put(newDevice.getDeviceId(), newDevice);
+    }
 }
