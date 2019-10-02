f84334c433432fd14257c4e4b6bf9570fbc0672e
RafaCanto <rafacantovaz@gmail.com>
2019-10-02 17:54:38 -0500

    Se agregaron las clases House, Area y Room

---
+++ mavenproject1/src/main/java/Room.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author Rafael
+ */
+public class Room {
+    private String roomName;
+    private String roomId;
+    Map<String, Device> Devices = new HashMap<>();
+    
+    public void setRoomName(String roomName){
+        this.roomName = roomName;
+    }
+    public void setRoomId(String roomId){
+        this.roomId = roomId;
+    }
+    public String getRoomName(){
+        return roomName;
+    }
+    public String getRoomId(){
+        return roomId;
+    }
+    
+    Room(String roomName, String roomId){
+        this.roomName = roomName;
+        this.roomId = roomId;
+    }
+    
+    public void addDevice(Device newDevice){
+        Devices.put(newDevice.getDeviceId(), newDevice);
+    }
+    
+    public void removeDevice(String deviceId) {
+        if(Devices.containsKey(deviceId)) {
+            Devices.remove(deviceId);
+        }
+    }
+}
