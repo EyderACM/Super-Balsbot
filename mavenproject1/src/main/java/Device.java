5764648c1392eac0267876cda38ccc4613ea8320
Edgar Aguilar <edgar@MacBook-Air-de-Edgar.local>
2019-10-03 08:48:25 -0500

    i changed the public atributes to private atributes

--- mavenproject1/src/main/java/Device.java
+++ mavenproject1/src/main/java/Device.java
@@ -9,12 +9,12 @@
  * @author edgar
  */
 public class Device {
-    public String deviceName;
-    public String deviceId;
-    public String areaId;
-    public String houseId;
-    public String roomId;
-    public String model;
+    private String deviceName;
+    private String deviceId;
+    private String areaId;
+    private String houseId;
+    private String roomId;
+    private String model;
     
     Device(String deviceName, String houseId, String areaId, String roomId,String deviceId, String model){
         this.houseId = houseId;
@@ -22,6 +22,40 @@ public class Device {
         this.roomId = roomId;
         this.deviceId = deviceId;
         this.model = model;
+    }
+    public void setDeviceName(String deviceName){
+        this.deviceName = deviceName;
+    }
+    public void setDeviceId(String deviceId){
+        this.deviceId = deviceId;
+    }
+    public void setAreaId(String areaId){
+        this.areaId = areaId;
+    }
+    public void setHouseId(String houseId){
+        this.houseId = houseId;
+    }
+    public void setRoomId(String roomId){
+        this.roomId = roomId;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    
+    public String getDeviceName(){
+        return deviceName;
+    }
+    public String getAreaId(){
+        return areaId;
+    }
+    public String getHouseId(){
+        return houseId;
+    }
+    public String getRoomId(){
+        return roomId;
+    }
+    public String getModel(){
+        return model;
     }
     
     public String turnOn(){
