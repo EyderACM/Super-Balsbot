73a0f6cd80cc3f290a9b9483913e4fde9b1b6e8e
Edgar Aguilar <edgar@MacBook-Air-de-Edgar.local>
2019-10-03 21:57:13 -0500

    los dispositivos se encienden y apagan

--- mavenproject1/src/main/java/FeatureHierarchy.java
+++ mavenproject1/src/main/java/FeatureHierarchy.java
@@ -21,30 +21,17 @@ public class FeatureHierarchy {
         Room Recamara = new Room("Rafa's Bedroom", "0");
         Room Sanitario = new Room("Rafa's Bathroom", "1");
         Room Sanitario2 = new Room("Edgar's Bathroom", "0");
-        //AcPanasonic Aire1 = new AcPanasonic("Aire feo", "0", "0", "0", "0", "Panasonic","Apagado");
-        //AcSamsung Aire2 = new AcSamsung("Aire de lujo", "0", "0", "0", "1", "Samsung", "Encendido");
+        AcPanasonic Aire1 = new AcPanasonic("Ac","Aire todo poderoso", "0", "0", "0", "0", "Panasonic", "Apagado");
+        AcSamsung Aire2 = new AcSamsung("Ac","Aire menos poderoso", "0", "0", "0", "0", "samsung", "encendido");
+       //String type, String deviceName, String houseId, String areaId, String roomId,String deviceId, String model, String status
+        Casa.addArea(Cuarto);
+        Casa.getArea("0").addRoom(Recamara);
+        Casa.getArea("0").getRoom("0").addDevice(Aire1);
+        Casa.getArea("0").getRoom("0").addDevice(Aire2);
         
-        //Casa.addArea(Cuarto);
-        //Casa.getArea("0").addRoom(Recamara);
-        //Casa.getArea("0").addRoom(Sanitario);
-        //Casa.getArea("0").addRoom("0").addDevice(Aire1);
-        //Casa.getArea("0").addRoom().addDevice(Aire2);
+        Casa.getArea("0").getRoom("0").turnOnDevices();
+        Casa.getArea("0").getRoom("0").turnOffDevices();
         
-        
-        System.out.println("Prueba 1:");
-        Casa.printAreas();
-        
-        System.out.println("Prueba 2:");
-        Casa.getArea("0").removeRoom("0");
-        Casa.printAreas();
-        
-        System.out.println("Prueba 3:");
-        Casa.getArea("0").addRoom(Sanitario2);
-        Casa.printAreas();
-        
-        System.out.println("Prueba 4:");
-        //Casa.getArea("0").getRoom("0").addDevice(Aire1);
-        Casa.printAreas();
         
     }
 
